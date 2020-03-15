94b9e617b0bad04589da27ab0daa13998080a7dd
Cam Cardosi <camcardosi@mac.com>
2020-03-15 16:30:28 -0700

    model tests passing

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,7 @@
 class Pizza < ApplicationRecord
+  # model association
+  has_many :pizza_toppings, dependent: :destroy
+
+  # validations
+  validates_presence_of :name, :description
 end
--- app/models/topping.rb
+++ app/models/topping.rb
@@ -1,2 +1,7 @@
 class Topping < ApplicationRecord
+  # model association
+  has_many :pizza_toppings, dependent: :destroy
+
+  # validations
+  validates_presence_of :name
 end
