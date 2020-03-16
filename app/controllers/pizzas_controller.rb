2e8beb719b29e6b53b7eeda5357261c8200b2464
Cam Cardosi <camcardosi@mac.com>
2020-03-15 18:03:57 -0700

    post pizza tests passing

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -4,4 +4,17 @@ class PizzasController < ApplicationController
     @pizzas = Pizza.all
     json_response(@pizzas)
   end
+
+  # POST /pizzas
+  def create
+    @pizza = Pizza.create!(pizza_params)
+    json_response(@pizza, :created)
+  end
+
+  private
+
+  def pizza_params
+    # whitelist params
+    params.permit(:name, :description)
+  end
 end
