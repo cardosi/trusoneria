1a90a434941fb5602fc92a9625f07cc41635dcda
Cam Cardosi <camcardosi@mac.com>
2020-03-15 18:21:45 -0700

    topping request spec passing

--- app/controllers/toppings_controller.rb
+++ app/controllers/toppings_controller.rb
@@ -1,2 +1,20 @@
 class ToppingsController < ApplicationController
+  # GET /toppings
+  def index
+    @toppings = Topping.all
+    json_response(@toppings)
+  end
+
+  # POST /toppings
+  def create
+    @topping = Topping.create!(topping_params)
+    json_response(@topping, :created)
+  end
+
+  private
+
+  def topping_params
+    # whitelist params
+    params.permit(:name)
+  end
 end
