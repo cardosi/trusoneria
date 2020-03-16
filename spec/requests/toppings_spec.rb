21caba8a8c1ed04aaaf17baf137ef5857b9fe47a
Cam Cardosi <camcardosi@mac.com>
2020-03-15 18:17:00 -0700

    added topping request spec

--- spec/requests/toppings_spec.rb
+++ spec/requests/toppings_spec.rb
@@ -1,2 +1,39 @@
-# Test GET  /toppings
-# Test POST /toppings
+require 'rails_helper'
+
+RSpec.describe 'Trusoneria API', type: :request do
+  # initialize test data
+  let!(:toppings) { create_list(:topping, 10) }
+
+  # Test GET /toppings
+  describe 'GET /toppings' do
+    # make HTTP get request before each example
+    before { get '/toppings' }
+
+    it 'returns toppings' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  # Test POST /toppings
+  describe 'POST /toppings' do
+    # valid payload
+    let(:valid_attributes) { { name: 'Ricotta' } }
+
+    context 'when the request is valid' do
+      before { post '/toppings', params: valid_attributes }
+
+      it 'creates a topping' do
+        expect(json['name']).to eq('Ricotta')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+  end
+end
